0710bd2e6eaa30ee76e69ef159c71e4b001f8a84
Philip Newborough <corenominal@corenominal.org>
2016-01-30 08:58:27 +0000

    Set-up plugin

---
+++ corenominal-wp-shortcodes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: corenominal's Custom Shortcodes
+ * Description: This plugin provides custom shortcodes for my site.
+ * Plugin URI: https://github.com/corenominal/corenominal-wp-shortcodes
+ * Author: Philip Newborough
+ * Author URI: http://corenominal.org
+ * Version: 0.0.1
+ * License: GPLv2
+ */
